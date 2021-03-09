23784b29e113df514769d88bc46c258efed98273
Hemil <hemil.desai10@gmail.com>
2021-03-09 08:54:27 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ucla263brah",
+    version="0.1",
+    packages=find_packages(),
+    python_requires=">=3.7, <4",
+    description="Source code by team UCLA263BRAH for SEMEVAL 2020 TASK 11 DETECTION OF PROPAGANDA TECHNIQUES IN NEWS ARTICLES",
+)
